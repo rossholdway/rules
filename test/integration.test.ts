dae0136a5cf937c8a32a0b4c34a22aaa882fb9c7
Ross Holdway <rossholdway@gmail.com>
2022-09-25 20:10:53 +0100

    Add new integration test

---
+++ test/integration.test.ts
@@ -0,0 +1,116 @@
+import { expect } from "https://cdn.skypack.dev/chai@4.3.4?dts";
+import { describe, it } from "https://deno.land/std@0.145.0/testing/bdd.ts";
+
+import {
+  parse,
+  isValid,
+  // Rules
+  any,
+  array,
+  bigInt,
+  bool,
+  enums,
+  literal,
+  num,
+  obj,
+  regex,
+  str,
+  tuple,
+  // utils
+  coerce,
+  defaulted,
+  dynamic,
+  intersection,
+  nullable,
+  optional,
+  refine,
+  union
+} from "../src/mod.ts";
+
+describe("integration", function () {
+
+  describe("Full object", function () {
+    it("should return as valid", function () {
+      const simpson = obj({
+        firstName: str(),
+        middleName: optional(str()),
+        lastName: intersection([
+          str(),
+          literal("Simpson")
+        ]),
+        other: any(),
+        episodes: array(obj({
+          id: str(),
+          name: str(),
+          number: num({ min: 1, max: 728 }),
+          season: num()
+        })),
+        ageInSeconds: bigInt(),
+        alive: defaulted(bool(), (_ctx) => true),
+        hairColour: enums(["Yellow", "Blue", "Bald", "Brown"] as const),
+        age: coerce(num(), (value) => {
+          if (typeof value === "string") {
+            return parseInt(value, 10);
+          } else {
+            return value;
+          }
+        }),
+        neighbour: regex(/.*Flanders$/i),
+        children: nullable(
+          tuple([literal("Bart"), literal("Lisa"), literal("Maggie")])
+        ),
+        email: refine("email", str(), (ctx) => {
+          if (!/\S+@\S+\.\S+/.test(ctx.value)) {
+            return ctx.error(
+              "invalid_email",
+              "Must be a valid email"
+            )
+          }
+          return ctx.success();
+        }),
+        knownAs: union([
+          literal("Max Power"),
+          literal("Mr. Plow"),
+          literal("Mr. Sparkle")
+        ])
+      });
+
+      const homer = {
+        firstName: "Homer",
+        lastName: "Simpson",
+        other: { favColour: "Yellow" },
+        episodes: [{
+          id: "3F14",
+          name: "Homer the Smithers",
+          number: 145,
+          season: 7
+        }],
+        ageInSeconds: 1229904000n,
+        hairColour: "Bald",
+        age: "39",
+        neighbour: "Ned Flanders",
+        children: ["Bart", "Lisa", "Maggie"],
+        email: "chunkylover53@aol.com",
+        knownAs: "Mr. Plow"
+      }
+
+      const result = parse(simpson, homer);
+
+      expect(isValid(result)).to.be.true;
+      expect(result[1]!).to.eql(  {
+        firstName: "Homer",
+        lastName: "Simpson",
+        other: { favColour: "Yellow" },
+        episodes: [ { id: "3F14", name: "Homer the Smithers", number: 145, season: 7 } ],
+        ageInSeconds: 1229904000n,
+        alive: true,
+        hairColour: "Bald",
+        age: 39,
+        neighbour: "Ned Flanders",
+        children: [ "Bart", "Lisa", "Maggie" ],
+        email: "chunkylover53@aol.com",
+        knownAs: "Mr. Plow"
+      });
+    });
+  });
+});
