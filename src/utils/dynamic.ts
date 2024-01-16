a38c3cfa3217e2d41ff0ad5a006476f49b884cc8
Ross Holdway <rossholdway@gmail.com>
2024-01-16 21:19:01 +0000

    Refactor dynamic typing

--- src/utils/dynamic.ts
+++ src/utils/dynamic.ts
@@ -1,19 +1,16 @@
 import { Context } from "../mod.ts";
 import type { Rule } from "../mod.ts";
 
+type ExtractValue<T> = T extends Rule<infer U> ? U : never;
+
 // deno-lint-ignore no-explicit-any
 export type dynamicFn<O> = (ctx: Context<any>) => O;
 
 // Help to decide what validation to run at runtime
 // deno-lint-ignore no-explicit-any
-export function dynamic<O extends Rule<any>>(decisionFn: dynamicFn<O>): O {
-
+export function dynamic<O extends dynamicFn<Rule<any>>>(decisionFn: O): Rule<ExtractValue<ReturnType<O>>> {
   return function dynamic(ctx) {
     const fn = decisionFn(ctx);
     return fn(new Context(fn.name, ctx.value, ctx.path, ctx.errors));
-  } as O;
+  };
 }
-
-// as 0 requried, otherwise we get the error assignable to the constraint
-// of type 'T', but 'T' could be instantiated with a different subtype of constraint.
-// Not sure how to solve this... moving on for now.
